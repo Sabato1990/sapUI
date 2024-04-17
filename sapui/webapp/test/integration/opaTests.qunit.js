887f2188d0ebb81bab3434ba80603e098b86b3ca
Sabato Cerciello <sabato.cerciello@accenture.com>
2024-04-17 10:51:26 +0000

    first commit

---
+++ sapui/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/webinar/sapui/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ sapui/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comwebinar/sapui/test/unit/controller/HomePage.controller"
+], function () {
+	"use strict";
+});
